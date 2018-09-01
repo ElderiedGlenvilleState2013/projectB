c0dd35515dc608da5dc2b46ee768d64bb1599d9b
Elderied McKinney <mckinneydemetrice@icloud.com>
2018-09-01 18:53:58 -0400

    first commit

---
+++ DoggZam!/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  DoggZam!
+//
+//  Created by McKinney family  on 9/1/18.
+//  Copyright © 2018 FasTek Technologies. All rights reserved.
+//
+
+import Foundation
---
+++ DoggZam!/Services/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  DoggZam!
+//
+//  Created by McKinney family  on 9/1/18.
+//  Copyright © 2018 FasTek Technologies. All rights reserved.
+//
+
+import Foundation
