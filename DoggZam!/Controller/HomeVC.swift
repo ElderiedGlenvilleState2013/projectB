1f0cc931e8c313bd985bde2d34e68b668a99fdac
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-01-12 15:21:33 -0500

    sidePanelVC
    adding sidePanel and messing VC

---
+++ DoggZam!/Controller/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  DoggZam!
+//
+//  Created by McKinney family  on 1/12/19.
+//  Copyright © 2019 FasTek Technologies. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DoggZam!/Controller/leftSidePanelVC.swift
@@ -0,0 +1,21 @@
+//
+//  leftSidePanelVC.swift
+//  DoggZam!
+//
+//  Created by McKinney family  on 1/12/19.
+//  Copyright © 2019 FasTek Technologies. All rights reserved.
+//
+
+import UIKit
+
+class leftSidePanelVC: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
